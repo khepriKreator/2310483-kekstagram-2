c97f0700ff15cc523e191eefa345e61f97d9f220
khepri_kreator <makareev.dima@yandex.ru>
2026-03-07 12:34:44 +0800

    added script to html, refactor getNumberFromString fnc

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+const validateStringLenght = (string, length) => (string.length <= length) ? true : false;
+
+// Через преобразование в массив и обратно
+const isStringPalindrome1 = (string) => {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = normalizedString.split('').reverse().join('');
+  return normalizedString === reversedString;
+};
+
+// Через цикл for без преобразования в массив
+const isStringPalindrome2 = (string) => {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return normalizedString === reversedString;
+};
+
+const getNumberFromString = (string) => {
+  const normalizedString = (typeof string === 'number') ? string.toString() : string;
+  let result = '';
+  for (let i = 0; i < normalizedString.length; i++) {
+    const symbol = normalizedString[i];
+    result = !isNaN(parseInt(symbol, 10)) ? result + symbol : result;
+  }
+  return (result === '') ? NaN : parseInt(result, 10);
+};
+
+console.log(getNumberFromString('123'));
+console.log(getNumberFromString('123 456'));
+console.log(getNumberFromString('dgbddasf sdgfjnakjdfnkj adfn'));
