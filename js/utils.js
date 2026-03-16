3d15ce891f2500ecd79f0a8d922576daecd86809
khepri_kreator <makareev.dima@yandex.ru>
2026-03-17 02:31:11 +0800

    managed open and close modal window

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,18 @@
-const validateStringLenght = (string, length) => string.length <= length;
+//Генерирует случайное число
+const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+//Проверяет длину строки
+const isStringLonger = (string, length) => string.length <= length;
 
 // Через преобразование в массив и обратно
-const isStringPalindrome1 = (string) => {
+/* const isStringPalindrome = (string) => {
   const normalizedString = string.toLowerCase().replaceAll(' ', '');
   const reversedString = normalizedString.split('').reverse().join('');
   return normalizedString === reversedString;
-};
+}; */
 
 // Через цикл for без преобразования в массив
-const isStringPalindrome2 = (string) => {
+const isStringPalindrome = (string) => {
   const normalizedString = string.toLowerCase().replaceAll(' ', '');
   let reversedString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
@@ -17,6 +21,7 @@ const isStringPalindrome2 = (string) => {
   return normalizedString === reversedString;
 };
 
+// Извлекает цифры из строки и преобразует в одно число
 const getNumberFromString = (string) => {
   const normalizedString = (typeof string === 'number') ? string.toString() : string;
   let result = '';
@@ -25,4 +30,17 @@ const getNumberFromString = (string) => {
     result = !isNaN(parseInt(symbol, 10)) ? result + symbol : result;
   }
   return (result === '') ? NaN : parseInt(result, 10);
+};
+
+// Проверяет событий клавиатуры
+const isEscapeKey = (evt) => evt.key === 'Escape' || evt.keyCode === 27;
+const isEnterKey = (evt) => evt.key === 'Enter' || evt.keyCode === 13;
+
+export {
+  getRandomNumber,
+  isStringLonger,
+  isStringPalindrome,
+  getNumberFromString,
+  isEscapeKey,
+  isEnterKey,
 };
